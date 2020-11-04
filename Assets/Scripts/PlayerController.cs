87eeeeb1d890a3d19f174a74a12083ee56c5fc8d
Hugo Dutka <dutkahugo@gmail.com>
2020-11-04 09:58:49 +0100

    first class

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D m_Rigidbody2D;
+
+    [SerializeField]
+    private float m_MovementSpeed = 3f;
+
+    [SerializeField]
+    private float m_JumpForce = 500f;
+
+    private bool m_Jump = false;
+
+    private float m_Movement = 0.0f;
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        m_Movement = Input.GetAxis("Horizontal") * m_MovementSpeed;
+
+        m_Jump = Input.GetButtonDown("Jump");
+    }
+
+    private void UpdateMovement()
+    {
+        m_Rigidbody2D.velocity = new Vector2(m_Movement, m_Rigidbody2D.velocity.y);
+
+        if (m_Jump)
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+}
