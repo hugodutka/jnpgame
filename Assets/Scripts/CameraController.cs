b0bb89c24aae4294eb5a77142897383657fa9435
Hugo Dutka <dutkahugo@gmail.com>
2020-11-18 09:58:18 +0100

    third class

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+
+    [SerializeField]
+    private float m_SmoothTime = 1.0f;
+
+    [SerializeField]
+    private Vector3 m_Offset;
+
+    private Vector3 m_Velocity;
+
+    void Update()
+    {
+        if (m_Target == null)
+        {
+            return;
+        }
+
+        Vector3 targetPostion = m_Target.position + m_Offset;
+
+        if (MathUtils.AreApproximatelyEqual(transform.position, targetPostion))
+        {
+            return;
+        }
+
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPostion, ref m_Velocity, m_SmoothTime);
+        transform.position = smoothPosition;
+    }
+}
