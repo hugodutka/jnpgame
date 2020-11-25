dac6366b4685964e55c02ddadf956221f468b8b5
Hugo Dutka <dutkahugo@gmail.com>
2020-11-25 10:00:10 +0100

    fourth class

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameController : ASingleton<GameController>
+{
+    private ulong m_Points = 0;
+
+    public void HandleCoinPickedUp(Coin coin)
+    {
+        if (coin == null)
+        {
+            return;
+        }
+
+        m_Points += coin.Points;
+        Debug.Log($"total points: {m_Points}");
+    }
+}
