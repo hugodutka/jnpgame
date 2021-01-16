d0678ed365c0e0607f348afb5c6ee762fa979aa0
Hugo Dutka <dutkahugo@gmail.com>
2021-01-16 22:36:39 +0100

    add an explosive, lethal bomb and platform

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,25 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class Saw : MonoBehaviour
+public class Bomb : MonoBehaviour
 {
     [SerializeField]
     private float m_ForceModifier = 5000f;
-
-    [SerializeField]
-    private Vector3 m_Speed;
-
-    private void Update()
-    {
-        transform.position += m_Speed * Time.deltaTime;
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController player = collision.GetComponent<PlayerController>();
         if (player != null)
         {
+            Explode();
             player.Kill();
             player.Rigidbody.AddForce(Vector3.up * m_ForceModifier);
         }
+    }
+
+    private void Explode()
+    {
+        GetComponent<Animator>().SetBool("Exploded", true);
     }
 }
